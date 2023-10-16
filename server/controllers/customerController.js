95054fe5a607cc09b8cf7eb1e2b1d22632826bcf
Pranav K <pranavkcse@gmail.com>
2023-10-16 10:26:01 +0530

    Completed

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -41,6 +41,31 @@ exports.homepage = async (req, res) => {
 
 }
 
+
+/**
+ * GET /
+ * About
+ */
+
+exports.about = async (req, res) => {
+
+    const locals = {
+        title: "NodeJs",
+        description: "NodeJs User Management System"
+    }
+    try {
+
+        res.render('about', {
+            locals,
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
 /**
  * GET /
  * New Customer Form
@@ -80,4 +105,137 @@ exports.postCustomer = async (req, res) => {
         console.log(error);
     }
 
+}
+
+/**
+ * GET /
+ * view details
+ */
+
+exports.viewCustomer = async (req, res) => {
+
+
+    try {
+        const locals = {
+            title: "NodeJs | View Customer Data",
+            description: "NodeJs User Management System"
+        }
+        const customer = await Customer.findOne({ _id: req.params.id })
+
+        res.render('customer/view', {
+            locals,
+            customer
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ * GET /
+ * Edit details
+ */
+
+exports.editCustomer = async (req, res) => {
+
+
+    try {
+        const locals = {
+            title: "NodeJs | Edit Customer Data",
+            description: "NodeJs User Management System"
+        }
+        const customer = await Customer.findOne({ _id: req.params.id })
+
+        res.render('customer/edit', {
+            locals,
+            customer
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+/**
+ * PUT /
+ * Edit details
+ */
+
+exports.editDetails = async (req, res) => {
+
+
+    try {
+        const locals = {
+            title: "NodeJs | Edit Customer Data",
+            description: "NodeJs User Management System"
+        }
+        await Customer.findByIdAndUpdate(req.params.id, {
+            firstName: req.body.firstName,
+            lastNames: req.body.lastName,
+            tel: req.body.tel,
+            email: req.body.email,
+            details: req.body.details,
+            updatedAt: Date.now(),
+        })
+
+        res.redirect(`/edit/${req.params.id}`)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+/**
+ * DELETE /
+ * Delete Customer
+ */
+
+exports.deleteCustomer = async (req, res) => {
+
+
+    try {
+
+        await Customer.deleteOne({ _id: req.params.id })
+
+        res.redirect(`/`)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ * GET /
+ * Search Customer
+ */
+
+exports.searchCustomer = async (req, res) => {
+
+    try {
+        const locals = {
+            title: "NodeJs | Search",
+            description: "NodeJs User Management System",
+        }
+
+        let searchTerm = req.body.searchTerm
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "")
+
+        const customers = await Customer.find({
+            $or: [
+                { firstName: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+                { lastName: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+            ]
+        });
+
+        res.render("search", {
+            customers,
+            locals
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -7,9 +7,15 @@ const customerController = require('../controllers/customerController');
  */
 
 router.get('/', customerController.homepage);
+router.get('/about', customerController.about);
 router.get('/add', customerController.addCustomer);
 router.post('/add', customerController.postCustomer);
 
+router.get('/view/:id', customerController.viewCustomer);
+router.get('/edit/:id', customerController.editCustomer);
+router.put('/edit/:id', customerController.editDetails);
+router.delete('/edit/:id', customerController.deleteCustomer);
 
+router.post('/search', customerController.searchCustomer);
 
 module.exports = router
